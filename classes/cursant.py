8f39ae4ef748ee6aae9772b1a38d9e2c7ec2e075
cojocariudanie1 <cojocariudaniel02@gmail.com>
2022-10-21 15:35:14 +0300

    Modificare def __init__. nr_ore si ore_finalizate au ca default valoarea 0. Adaugare atribut ore_finalizate

--- classes/cursant.py
+++ classes/cursant.py
@@ -13,10 +13,13 @@ class Cursant(Base):
     pachet_ore = relationship("PachetOre")
     pachet_ore_id = Column(Integer, ForeignKey("pachet_ore.id"))
     nr_ore = Column(Integer)
+    ore_finalizate = Column(Integer)
     cont = relationship("Cont", back_populates="cursant")
     cont_id = Column(Integer, ForeignKey("cont.id"))
 
-    def __init__(self, nume, prenume, dataNasterii):
+    def __init__(self, nume, prenume, dataNasterii, nr_ore = 0, ore_finalizate=0):
         self.nume = nume
         self.prenume = prenume
         self.dataNasterii = dataNasterii
+        self.ore_finalizate = ore_finalizate
+        self.nr_ore = nr_ore
