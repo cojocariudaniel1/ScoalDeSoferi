4cadd5cb9280e1df6af74f19b83fce5c4e221efa
Cojocariu Daniel <56277656+cojocariudaniel1@users.noreply.github.com>
2022-12-13 18:58:45 +0200

    Delete VSCode directory

--- VSCode/Chess/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from model.chess_window import ChesWindow
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    widget = ChesWindow()
-    widget.show()
-
-    app.exec_()
